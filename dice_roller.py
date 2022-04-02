885f50b10b93e95f34d593febb12503a250939f3
srps <a_k_a_bouncer@hotmail.com>
2022-04-02 23:56:55 +0100

    now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,17 @@
+import random
+
 def main():
-  print('You rolled a die')
+
+  # Get the number of dice to roll
+  dice_rolls = 2
+  dice_sum = 0
+  for i in range(dice_rolls):
+    # Roll the dice
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+
+  print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
